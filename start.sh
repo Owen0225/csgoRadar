f0bcb822cb33d6e746ef1ebed34ac357e5907850
Object <xiaowei@chinabaiker.com>
2023-05-09 06:07:32 +0800

    update

--- start.sh
+++ start.sh
@@ -1,14 +1,4 @@
 #!/bin/sh
 
-while true
-do
-ps -ef | grep "node index.js" | grep -v "grep"
-if [ "$?" -eq 1 ]
-then
-node index.js
-echo "process has been restarted!"
-else
-echo "process already started!"
-fi
-sleep 10
-done
+screen node index.js
+screen -S csgoRadar -X stuff "node index.js\n"
